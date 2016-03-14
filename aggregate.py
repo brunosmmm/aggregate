6b73f76970472df718473439b3376a44ae6c6414
brunosmmm <brunosmmm@gmail.com>
2016-03-14 13:11:51 -0400

    simple node discovery works

--- aggregate.py
+++ aggregate.py
@@ -1,4 +1,5 @@
 from discover.discover import AvahiDiscoverLoop
+from node.scan import scan_new_node
 from node.node import PeriodicPiNode
 import signal
 import avahi
@@ -6,9 +7,12 @@ import re
 
 PERIODIC_PI_NODE_REGEX = re.compile(r'^PeriodicPi node \[([a-zA-Z]+)\]')
 
+class DuplicateNodeError(Exception):
+    pass
+
 class PeriodicPiAgg(object):
     def __init__(self, filter_iface=None):
-        self.active_nodes = []
+        self.active_nodes = {}
         self.listen_iface = filter_iface
 
     def discover_new_node(self, **kwargs):
@@ -28,8 +32,20 @@ class PeriodicPiAgg(object):
         if m == None:
             return
 
+        #duplicate node (add to logging when available)
+        if m.group(1) in self.active_nodes:
+            return
+
         #add node
-        self.active_nodes.append(PeriodicPiNode(m.group(1), [kwargs['address'], kwargs['port']]))
+        new_node = PeriodicPiNode(m.group(1), [kwargs['address'], kwargs['port']])
+        #scan
+        new_node.register_basic_information(**scan_new_node(new_node.addr))
+
+        self.active_nodes[m.group(1)] = new_node
+
+        print 'new node = {} ({}) at {}'.format(new_node.node_element,
+                                                new_node.description,
+                                                new_node.location)
 
     def remove_node(self, **kwargs):
 
