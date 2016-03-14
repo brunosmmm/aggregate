5c916017fd596ebf28fd65a7e6392e03e79b9b0b
brunosmmm <brunosmmm@gmail.com>
2016-03-14 16:38:04 -0400

    create lirc client instance

--- plugins/lircd.py
+++ plugins/lircd.py
@@ -1,11 +1,17 @@
 from node.service.driver import NodeServiceDriver, NodeServiceDriverArgument, DriverCapabilities
+from periodicpy.irtools.lirc import LircClient
 
 class LircdDriver(NodeServiceDriver):
     _module_desc = NodeServiceDriverArgument('lircd', 'lircd client driver')
     _capabilities = [DriverCapabilities.MultiInstanceAllowed]
+    _required_kw = [NodeServiceDriverArgument('server_address', 'lircd server address'),
+                    NodeServiceDriverArgument('server_port', 'lircd server port')]
 
     def __init__(self, **kwargs):
-        super(LircdDriver, self).__init__()
+        super(LircdDriver, self).__init__(**kwargs)
+
+        self.lirc_handler = LircClient(self._loaded_kwargs['server_address'],
+                                       self._loaded_kwargs['server_port'])
 
 def discover_module():
     return LircdDriver
