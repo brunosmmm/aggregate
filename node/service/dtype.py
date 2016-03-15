41c0f83351ac3687780fd4b4a4b5d67fb70c3395
brunosmmm <brunosmmm@gmail.com>
2016-03-15 13:42:24 -0400

    add data types

---
+++ node/service/dtype.py
@@ -0,0 +1,10 @@
+#data types for completeness
+class PPDataTypes:
+    INT = 0
+    FLOAT = 1
+    STRING = 2
+    VOID = 3
+    INT_LIST = 4
+    FLOAT_LIST = 5
+    STRING_LIST = 6
+    VOID_LIST = 7
