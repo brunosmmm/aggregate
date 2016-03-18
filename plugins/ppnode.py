1419ad459a504846e8260a69ee97ca58d9419a09
brunosmmm <brunosmmm@gmail.com>
2016-03-17 23:09:00 -0400

    use proper hook scheme

--- plugins/ppnode.py
+++ plugins/ppnode.py
@@ -33,7 +33,12 @@ class PPNodeDriver(Module):
                                                                  'method_args' : ModuleMethodArgument(argument_desc='Method arguments',
                                                                                                       required=False,
                                                                                                       data_type=ModuleDataTypes.DICT)},
-                                                    method_return=ModuleDataTypes.VOID)}
+                                                    method_return=ModuleDataTypes.VOID),
+                'inspect_plugin' : ModuleMethod(method_desc='Inspect plugin structure',
+                                                method_args={'instance_name' : ModuleMethodArgument(argument_desc='Plugin instance name',
+                                                                                                    required=True,
+                                                                                                    data_type=ModuleDataTypes.STRING)},
+                                                method_return=ModuleDataTypes.DICT)}
 
     def __init__(self, **kwargs):
         super(PPNodeDriver, self).__init__(**kwargs)
@@ -44,16 +49,17 @@ class PPNodeDriver(Module):
 
         #get available drivers
         driver_list = self.interrupt_handler('get_available_drivers')
-        self.node.register_services(driver_list, kwargs['drvman'])
+        self.node.register_services(driver_list, kwargs['plugmgr'])
 
         #get plugin information, build structures
+        self.node.register_node_plugins()
 
         #connect properties, methods
         self._automap_properties()
         self._automap_methods()
 
         #add to active
-        self.interrupt_handler(call_custom_hook=['ppagg.add_node', [m.group(1), self.node]])
+        self.interrupt_handler(call_custom_method=['ppagg.add_node', [m.group(1), self.node]])
         #done
         self.interrupt_handler(log_info='new Periodic Pi node: {}'.format(m.group(1)))
 
@@ -66,6 +72,9 @@ class PPNodeDriver(Module):
 
     def _call_plugin_method(self, instance_name, method_name, method_args=None):
         return self.node.call_plugin_method(instance_name, method_name, method_args)
+
+    def _inspect_plugin(self, instance_name):
+        return self.node.get_node_plugin_structure(instance_name)
 
     @classmethod
     def new_node_detected(cls, **kwargs):
@@ -80,7 +89,7 @@ class PPNodeDriver(Module):
 def discover_module(module_manager_object):
     #install driver loading hook
     try:
-        module_manager_object.install_driver_hook('drvman.new_node',
+        module_manager_object.attach_custom_hook('ppagg.node_discovered',
                                                   PPNodeDriver.new_node_detected,
                                                   ModuleManagerHookActions.LOAD_MODULE,
                                                   PPNodeDriver)
